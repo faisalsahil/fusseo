b55f6a8fbd24b8bde1808cbb81cabe866644a7c2
usama.ashraf <usama.ashraf.khan@gmail.com>
2016-05-17 16:49:10 +0500

    production change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  config.action_cable.url = 'ws://fusseo-usama.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://fusseo-usama.herokuapp.com', /http:\/\/fusseo-usama.herokuapp.*/ ]
+  config.action_cable.url = 'ws://fusseo.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://fusseo.herokuapp.com', /http:\/\/fusseo.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
